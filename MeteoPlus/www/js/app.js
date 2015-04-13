0d24bf5a8d0c62d02fd432411af3248f85273f85
Xavier DONO <xavier.dono@gmail.com>
2015-04-13 22:08:15 +0200

    Initial commit

---
+++ MeteoPlus/www/js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+document.addEventListener('deviceready', function () {
+
+}, false);
+
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'partials/home.html'
+        })
+        .when('/about', {
+            templateUrl: 'partials/about.html'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+});
+
+app.controller('NavCtrl', function ($scope) {
+    $scope.menu = "home";
+});
+
+app.controller('WeatherCtrl', function ($scope, $http) {
+
+    $scope.Math = Math;
+    $scope.city = "Montpellier";
+
+    $scope.search = function () {
+        var url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.city + "&mode=json&units=metric&cnt=10";
+        $scope.loader = true;
+        $http.get(url).success(function (response) {
+            $scope.loader = false;
+            $scope.weather = response;
+        }).error(function () {
+            $scope.loader = false;
+            alert('Impossible de récupérer les informations !');
+        });
+    };
+});
